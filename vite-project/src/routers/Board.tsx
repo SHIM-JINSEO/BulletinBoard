9a66edad4641a9d72034a371442cf11ff75a3397
SHIM-JINSEO <sjs011828@gmail.com>
2024-09-07 14:40:13 +0900

    feat: adding delete button to delete board by using mutation

--- vite-project/src/routers/Board.tsx
+++ vite-project/src/routers/Board.tsx
@@ -1,11 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useBorads } from "../Context/BoardsContext";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 export default function Board() {
+  const queryClient = useQueryClient();
   const params = useParams();
-  
   const boards = useBorads();
-  console.log(boards);
+  const naviagte = useNavigate();
+  const token = localStorage.getItem("tokens");
   type board = {
     id: string;
     title: string;
@@ -17,20 +19,35 @@ export default function Board() {
       createdAt: string;
     };
   };
-  let currnetBorad: board | undefined = undefined;
+  let currentBorad: board | undefined = undefined;
   boards.forEach((board: board) => {
     if (board.id === params.boardId) {
-      currnetBorad = board;
+      currentBorad = board;
     }
   });
-  function handleClick(){
-    console.log("clicked!")
-  }
-  if (currnetBorad) {
+  const deleteBoard = useMutation({
+    mutationFn: (id: string) =>
+      axios.delete(`/api/boards/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    onSuccess() {
+      queryClient.invalidateQueries({queryKey: ["boards"]})
+      naviagte("../");
+    },
+  });
+  if (currentBorad) {
     return (
       <>
-        <h1>{currnetBorad.title}</h1>
-        <button onClick={handleClick}>Delete this board</button>
+        <h1>{currentBorad.title}</h1>
+        <input
+          type="button"
+          value="Delete this board"
+          onClick={() => {
+            deleteBoard.mutate(params.boardId);
+          }}
+        />
       </>
     );
   } else {
