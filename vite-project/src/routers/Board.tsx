9819a1264626eb32e53cd07a604354fd5c33e5a2
SHIM-JINSEO <sjs011828@gmail.com>
2024-09-07 16:58:34 +0900

    feat: Make components that create new postings(<Posting/>) and show each posting in board page(<PostingList/>)

--- vite-project/src/routers/Board.tsx
+++ vite-project/src/routers/Board.tsx
@@ -1,13 +1,100 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useBorads } from "../Context/BoardsContext";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+
+const token = localStorage.getItem("tokens");
+
+function Posting() {
+  const queryClient = useQueryClient();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [tag, setTag] = useState("");
+  const params = useParams();
+  const postPostings = useMutation({
+    mutationFn: () =>
+      axios.post(
+        `/api/posts?boardUuid=${params.boardId}`,
+        { title: title, body: body, tags: [tag] },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      ),
+    onSuccess() {
+      console.log("post success");
+      queryClient.invalidateQueries({ queryKey: ["postings"] });
+    },
+  });
+
+  return (
+    <>
+      <h2>Posting</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          postPostings.mutate();
+        }}
+      >
+        <label>Title</label>
+        <br />
+        <input type="text" onChange={(event) => setTitle(event.target.value)} />
+        <br />
+        <label>body</label>
+        <br />
+        <textarea onChange={(event) => setBody(event.target.value)} />
+        <br />
+        <label>Tag</label>
+        <br />
+        <input type="text" onChange={(event) => setTag(event.target.value)} />
+        <br />
+        <input type="submit" value="Posting" />
+      </form>
+    </>
+  );
+}
+function PostingList() {
+  const params = useParams();
+  const getPostings = useQuery({
+    queryKey: ["postings"],
+    queryFn: () => axios.get(`/api/posts?boardUuid=${params.boardId}`),
+  });
+  const { data: postings, error } = getPostings;
+  if (!error && postings) {
+    return (
+      
+      <>
+        <h2>Posting List</h2>
+        {
+          postings.data.list.map((posting)=>{
+            return <div key={posting.id} style={{border:"1px solid grey"}}>
+              <p>Title: {posting.title}</p>
+              <p>CreateAt: {posting.createdAt}</p>
+              <p>CreatedBy: {posting.createdBy.nickname}</p>
+              <p>Body</p>
+              <p>{posting.body}</p>
+              <input type="button" value="Delete this posting"></input>
+            </div>
+          })
+        }
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Fail to load postings!!!</h1>
+      </>
+    );
+  }
+}
 export default function Board() {
   const queryClient = useQueryClient();
   const params = useParams();
   const boards = useBorads();
   const naviagte = useNavigate();
-  const token = localStorage.getItem("tokens");
+
   type board = {
     id: string;
     title: string;
@@ -33,7 +120,7 @@ export default function Board() {
         },
       }),
     onSuccess() {
-      queryClient.invalidateQueries({queryKey: ["boards"]})
+      queryClient.invalidateQueries({ queryKey: ["boards"] });
       naviagte("../");
     },
   });
@@ -48,6 +135,8 @@ export default function Board() {
             deleteBoard.mutate(params.boardId);
           }}
         />
+        <Posting />
+        <PostingList />
       </>
     );
   } else {
